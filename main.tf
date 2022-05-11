126a27a601293b74277ee7474d72d3f86386a58e
Kulbhushan Mayer <kulbhushan.mayer@gmail.com>
2022-05-11 12:58:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,3 +14,42 @@ resource "null_resource" "install_zabbix_agent" {
     ]
   }
 }
+resource "null_resource" "update_zabbix_agentd_conf" {
+  provisioner "local-exec" {
+    command = "echo Server=${var.zabbix_server_ip} >> zabbix_agentd.conf && echo Server=${var.zabbix_server_hostname} >> zabbix_agentd.conf"
+  }
+}
+resource "null_resource" "copy_zabbix_agentd_conf" {
+  depends_on = [
+    null_resource.install_zabbix_agent,
+    null_resource.update_zabbix_agentd_conf
+  ]
+  provisioner "file" {
+    connection {
+      type = "ssh"
+      user = var.user
+      private_key = var.private_key
+      host = var.host
+    }
+    source = "zabbix_agentd.conf"
+    destination = "~/zabbix_agentd.conf"
+  }
+}
+resource "null_resource" "start_zabbix_agent" {
+  depends_on = [
+    null_resource.copy_zabbix_agentd_conf
+  ]
+  provisioner "remote-exec" {
+    connection {
+      type = "ssh"
+      user = var.user
+      private_key = var.private_key
+      host = var.host
+    }
+    inline = [
+      "sudo cp ~/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf",
+      "sudo systemctl restart zabbix-agent",
+      "sudo systemctl enable zabbix-agent"
+    ]
+  }
+}
